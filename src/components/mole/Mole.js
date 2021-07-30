aae7216c590b314613d6fcd94b93fb1cfae449f8
ptatro <6535799+ptatro@users.noreply.github.com>
2021-07-29 21:24:36 -0500

    working solution

--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -3,10 +3,24 @@ import './Mole.css'
 import MoleIcon from './Mole.svg'
 
 class Mole extends Component {
+  
+  display_mole() {
+    if(this.props.visible) {
+      console.log("passed true")
+      return (
+        <img src={MoleIcon} className="Mole" alt="Mole" onClick={this.props.moleWhacked}/>
+      )
+    } else {
+      console.log("passed false")
+    }
+  }
+  
   render() {
+    console.log(this.props.visible);
+    
     return (
       <div className="den">
-        <img src={MoleIcon} className="Mole" alt="Mole" />
+        {this.display_mole()}
       </div>
     )
   }
