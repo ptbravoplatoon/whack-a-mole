d37ba734f962f8c9f187ba9a7ff1ce52c208893b
jpeart03 <james.r.peart@gmail.com>
2021-07-29 20:25:00 -0500

    Jeff & Jim solution

--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -5,8 +5,10 @@ import MoleIcon from './Mole.svg'
 class Mole extends Component {
   render() {
     return (
-      <div className="den">
-        <img src={MoleIcon} className="Mole" alt="Mole" />
+      <div className="den"> 
+        {this.props.isMoleVisible &&
+          <img src={MoleIcon} className="Mole" alt="Mole" onClick={this.props.handleClick} />
+        }
       </div>
     )
   }
