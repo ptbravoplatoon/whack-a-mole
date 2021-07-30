91bba0a9e7410cfa1a50b32f56d220fec46b7a55
johnlillge <81765408+johnlillge@users.noreply.github.com>
2021-07-29 19:59:58 -0500

    initial working no comments

--- src/App.js
+++ src/App.js
@@ -1,51 +1,49 @@
-import React, { Component } from 'react'
-import './App.css'
-import Mole from './components/mole/Mole.js'
+import React, { Component } from 'react';
+import './App.css';
+import Mole from './components/mole/Mole.js';
 
 class App extends Component {
-  state = {
-    dens: this.getDensState(),
-    points: 0,
-  }
-  componentDidMount() {
-    this.startGame()
-  }
-  startGame() {
-    setInterval(() => {
-      this.setState({
-        dens: this.getDensState()
-      })
-    }, 1500)
-  }
-  getDensState() {
-    return new Array(9).fill({}).map(() => {
-      return { 
-        isMoleVisible: [true,false][Math.round(Math.random())] 
-      }
-    })
-  }
-  onMoleWhacked() {
-    this.setState({
-      points: this.state.points + 1
-    })
-  }
-  render() {
-    const dens = this.state.dens.map((den, index) => {
-      return (
-        <Mole key={`mole-${index}`} />
-      )
-    })
-    return (
-      <div className="App">
-        <h1>WHACK-A-MOLE!</h1>
-        <h2>Points: {this.state.points}</h2>
-        <div className="dens">
-          {dens}
-          <div style={{clear: 'both'}}></div>
-        </div>
-      </div>
-    )
-  }
+	state = {
+		dens: this.getDensState(),
+		points: 0
+	};
+	componentDidMount() {
+		this.startGame();
+	}
+	startGame() {
+		setInterval(() => {
+			this.setState({
+				dens: this.getDensState()
+			});
+		}, 1500);
+	}
+	getDensState() {
+		return new Array(9).fill({}).map(() => {
+			return {
+				isMoleVisible: [ true, false ][Math.round(Math.random())]
+			};
+		});
+	}
+	onMoleWhacked = () => {
+		this.setState({
+			points: this.state.points + 1
+		});
+	};
+	render() {
+		const dens = this.state.dens.map((den, index) => {
+			return <Mole key={`mole-${index}`} isVisible={den.isMoleVisible} onMoleWhacked={this.onMoleWhacked} />;
+		});
+		return (
+			<div className="App">
+				<h1>WHACK-A-MOLE!</h1>
+				<h2>Points: {this.state.points}</h2>
+				<div className="dens">
+					{dens}
+					<div style={{ clear: 'both' }} />
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
--- src/components/mole/Mole.js
+++ src/components/mole/Mole.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import './Mole.css'
-import MoleIcon from './Mole.svg'
+import React, { Component } from 'react';
+import './Mole.css';
+import MoleIcon from './Mole.svg';
 
 class Mole extends Component {
-  render() {
-    return (
-      <div className="den">
-        <img src={MoleIcon} className="Mole" alt="Mole" />
-      </div>
-    )
-  }
+	render() {
+		if (this.props.isVisible === true) {
+			return (
+				<div className="den" onClick={this.props.onMoleWhacked}>
+					<img src={MoleIcon} className="Mole" alt="Mole" />
+				</div>
+			);
+		} else {
+			return <div className="den" />;
+		}
+	}
 }
 
-export default Mole
+export default Mole;
