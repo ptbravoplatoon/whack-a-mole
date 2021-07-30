e4e1e0d9987cc692ee3f6794579775a013da80d8
johnlillge <81765408+johnlillge@users.noreply.github.com>
2021-07-29 20:36:52 -0500

    comments added

--- src/App.js
+++ src/App.js
@@ -7,9 +7,16 @@ class App extends Component {
 		dens: this.getDensState(),
 		points: 0
 	};
+
+	// componentDidMount() is invoked immediately after a component is mounted (inserted into the tree).
+	// Initialization that requires DOM nodes should go here.
+	// If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
 	componentDidMount() {
 		this.startGame();
 	}
+
+	// when the App is mounted this will execute since it is called in componentDidMount
+	// will call getDensState and call it again every 1.5 seconds
 	startGame() {
 		setInterval(() => {
 			this.setState({
@@ -17,6 +24,9 @@ class App extends Component {
 			});
 		}, 1500);
 	}
+
+	// creates a new array of nine items
+	// creates an object inside of each item with a isMoleVisible key and either a true or false value
 	getDensState() {
 		return new Array(9).fill({}).map(() => {
 			return {
@@ -24,6 +34,8 @@ class App extends Component {
 			};
 		});
 	}
+
+	// sets the points state to the previous points state value + 1
 	onMoleWhacked = () => {
 		this.setState({
 			points: this.state.points + 1
